8e17c89c7de013743f26d24e8f9fb125c8481581
Niko Fox <niko@fauxnik.me>
2020-08-07 17:15:13 -0700

    reformat Settings' Draw annotations

--- Settings.cs
+++ Settings.cs
@@ -4,13 +4,15 @@ namespace DvMod.CustomFeeValidator
 {
     public class Settings : UnityModManager.ModSettings, IDrawable
     {
-        [Draw("Enable logging")] public bool isLoggingEnabled =
+        [Draw("Enable logging")]
+        public bool isLoggingEnabled =
 #if DEBUG
             true;
 #else
             false;
 #endif
-        [Draw(Label = "Fee Validation Type", Type = DrawType.ToggleGroup, Vertical = true)] public FeeValidationType selectedValidationType = FeeValidationType.LastLoco;
+        [Draw(Label = "Fee Validation Type", Type = DrawType.ToggleGroup, Vertical = true)]
+        public FeeValidationType selectedValidationType = FeeValidationType.LastLoco;
 
         override public void Save(UnityModManager.ModEntry entry)
         {
